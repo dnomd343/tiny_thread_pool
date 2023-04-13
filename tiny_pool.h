96b9c05a8c8a29ff377734f1ec1d434c5aed650a
Dnomd343 <i@343.re>
2023-04-13 14:47:46 +0800

    update: enhance TinyPool module

--- tiny_pool.h
+++ tiny_pool.h
@@ -1,12 +1,36 @@
 #pragma once
 
 #include <future>
+#include <stdexcept>
 #include <functional>
 #include "tiny_pool/tiny_pool.h"
 
+class TinyPoolException : public std::runtime_error {
+public:
+    explicit TinyPoolException(const std::string &msg) : std::runtime_error(msg) {}
+    ~TinyPoolException() noexcept override = default;
+};
+
 class TinyPool { // OOP for tiny_thread_pool
 
-    pool_t *pool; // thread pool c-api entry
+    pool_t *pool_ = nullptr; // thread pool c-api entry
+
+    static pool_t* create_pool(uint32_t size) {
+        auto pool = tiny_pool_create(size);
+        if (pool == nullptr) {
+            throw TinyPoolException("tiny thread pool create failed");
+        }
+        return pool;
+    }
+
+    pool_t* transfer_pool() {
+        if (pool_ == nullptr) {
+            throw TinyPoolException("tiny thread pull already destroyed");
+        }
+        auto pool = pool_;
+        pool_ = nullptr; // clear class member
+        return pool;
+    }
 
     static void wrap_c_func(void *func) { // wrap std::function as c-style function ptr
         auto func_ptr = static_cast<std::function<void()>*>(func);
@@ -21,16 +45,26 @@ public:
         return (num == 0) ? 1 : num; // return 1 when fetch failed
     }
 
-    /// Create thread pool of a specified size.
-    explicit TinyPool(uint32_t size) {
-        pool = tiny_pool_create(size);
+    /// Create new thread pool.
+    TinyPool() { pool_ = create_pool(cpu_core_num()); }
+    explicit TinyPool(uint32_t size) { pool_ = create_pool(size); }
+
+    /// Start running the thread pool.
+    void boot() {
+        if (pool_ == nullptr) {
+            throw TinyPoolException("tiny thread pull already destroyed");
+        }
+        tiny_pool_boot(pool_);
     }
 
-    // TODO: free thread pool when failed
-    void boot() { tiny_pool_boot(pool); }
-    void join() { tiny_pool_join(pool); }
-    void kill() { tiny_pool_kill(pool); }
-    void detach() { tiny_pool_detach(pool); }
+    /// Join all threads.
+    void join() { tiny_pool_join(transfer_pool()); }
+
+    /// Force quit all threads.
+    void kill() { tiny_pool_kill(transfer_pool()); }
+
+    /// Detach all threads.
+    void detach() { tiny_pool_detach(transfer_pool()); }
 
     /// Submit new task to the thread pool.
     template <typename Func, typename ...Args>
@@ -39,11 +73,15 @@ public:
         auto func_ptr = std::make_shared<std::packaged_task<return_type()>>(
             std::bind(std::forward<Func>(func), std::forward<Args>(args)...) // wrap as a function without params
         );
-        tiny_pool_submit(pool, TinyPool::wrap_c_func, (void*)( // using thread pool interface for submit
+        tiny_pool_submit(pool_, TinyPool::wrap_c_func, (void*)( // using thread pool interface for submit
             new std::function<void()> (
                 [func_ptr]() { (*func_ptr)(); } // wrapped as lambda for running task
             )
         ));
         return func_ptr->get_future(); // return future object
+    }
+
+    ~TinyPool() {
+        if (pool_ != nullptr) { kill(); }
     }
 };
